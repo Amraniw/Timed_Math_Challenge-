d450eab4b6403600342a3d3c4e917caaa82b037b
Wassim Amrani <wassimamrani@live.fr>
2026-05-19 17:29:22 -0400

    generate random maths problems

---
+++ Timed_Math_Challenge.py
@@ -0,0 +1,22 @@
+import random
+
+OPERATORS = ["+", "-", "*"]
+MIN_OPERAND = 3
+MAX_OPERAND = 12
+TOTAL_PROBLEMS = 10
+
+def generate_problem():
+    left = random.randint(MIN_OPERAND, MAX_OPERAND)
+    right = random.randint(MIN_OPERAND, MIN_OPERAND)
+    operator = random.choice(OPERATORS)
+
+    expr = str(left) + " " + operator + " " + str(right)
+    answer = eval(expr)
+    return expr, answer 
+
+expr, answer = generate_problem()
+print(expr, answer)
+
+
+for i in range(TOTAL_PROBLEMS):
+    expr, answer = generate_problem()
